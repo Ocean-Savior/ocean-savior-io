0bb375cfc9dffcb3f5b0340631ceeacad562e0b0
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-07-25 12:59:25 +0500

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Intro from "./components/Intro/Intro";
+import Problem from "./components/Problem/Problem";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Intro />
       <About />
+      <Problem />
       <Footer />
     </div>
   );
---
+++ src/components/Problem/Problem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './Problem.module.scss'
+
+const Problem = () => {
+    return (
+        <section id="problem" className={styles.problem}>
+            <div className={`container ${styles.body}`}>
+                <h2 className={styles.title}>The problem of ocean pollution:</h2>
+                <p className={styles.p}>Ocean pollution has become one of the most serious environmental problems of our time. Every year, millions of tons of plastic and other waste end up in the world's oceans, posing a threat to marine life and humanity. Cleaning and restoring the marine environment require significant efforts and resources on a global scale. OceanSavior (OSR) offers an innovative approach to addressing the issue of ocean pollution. Through blockchain technology and cryptocurrency, we provide transparency, efficiency, and flexibility in funding environmental projects and initiatives. OceanSavior (OSR) facilitates the mobilization of funds, community involvement, and resource allocation for maximum environmental impact.</p>
+                <div className={styles.row}>
+                    <a href="#presale" className={`btn ${styles.btn}`}>Presale</a>
+                    <a href="#community" className={`btn2 ${styles.btn2}`}>Join the community</a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Problem
