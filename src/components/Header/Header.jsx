a30659b6a3ad2f306196064457f3d70ac8621196
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-07-25 11:52:51 +0500

    init

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './Header.module.scss';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <nav className={`container ${styles.nav}`}>
+                <a href="/" className={styles.logo}>
+                    <img src="/logo.png" alt=""/>
+                    <span>OSR</span>
+                </a>
+                <ul className={styles.list}>
+                    <li className={styles.li}>
+                        <a href="#tokenomics" className={styles.link}>Tokenomics</a>
+                    </li>
+                    <li className={styles.li}>
+                        <a href="#whitepaper" className={styles.link}>Whitepaper</a>
+                    </li>
+                    <li className={styles.li}>
+                        <a href="#mission" className={styles.link}>Mission</a>
+                    </li>
+                </ul>
+                <a href="#connect" className={`btn ${styles.btn}`}>Connect Wallet</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Intro.module.scss'
+
+const Intro = () => {
+    return (
+        <section className={styles.intro}>Intro</section>
+    )
+}
+
+export default Intro
