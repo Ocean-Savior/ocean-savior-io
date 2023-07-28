5d0dbeb4c742f083734b69bd2f2d92e8fda23e0e
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-07-28 15:23:08 +0500

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Header.module.scss';
 
 const Header = () => {
+    const [menuOpened, setMenuOpened] = useState(false);
+
+    const setMenu = () => {
+        setMenuOpened(!menuOpened)
+        document.body.classList.toggle('oh')
+    }
+
     return (
         <header className={styles.header}>
             <nav className={`container ${styles.nav}`}>
@@ -9,18 +16,21 @@ const Header = () => {
                     <img src="/logo.png" alt=""/>
                     <span>OSR</span>
                 </a>
-                <ul className={styles.list}>
+                <ul className={`${styles.list} ${menuOpened ? styles.open : ''}`}>
                     <li className={styles.li}>
-                        <a href="#tokenomics" className={styles.link}>Tokenomics</a>
+                        <a href="#tokenomics" className={styles.link} onClick={() => setMenu()}>Tokenomics</a>
                     </li>
                     <li className={styles.li}>
-                        <a href="#presale" className={styles.link}>Whitepaper</a>
+                        <a href="#presale" className={styles.link} onClick={() => setMenu()}>Whitepaper</a>
                     </li>
                     <li className={styles.li}>
-                        <a href="#mission" className={styles.link}>Mission</a>
+                        <a href="#mission" className={styles.link} onClick={() => setMenu()}>Mission</a>
                     </li>
                 </ul>
                 <a href="#connect" className={`btn ${styles.btn}`}>Connect Wallet</a>
+                <button className={`${styles.button} ${menuOpened ? styles.open : ''}`} onClick={() => setMenu()}>
+                    <span className={styles.line}></span>
+                </button>
             </nav>
         </header>
     )
