a6a3967c8471451591777ab894c6e736e33452ce
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-07-28 14:51:43 +0500

    init

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -5,7 +5,15 @@ const Clients = () => {
     return (
         <section className={styles.clients} id="clients">
             <div className={`container ${styles.body}`}>
-                <h2 className={styles.title}></h2>
+                <img src="/client-1.png" alt="" draggable={false}/>
+                <img src="/client-2.png" alt="" draggable={false}/>
+                <img src="/client-3.png" alt="" draggable={false}/>
+                <img src="/client-4.png" alt="" draggable={false}/>
+                <img src="/client-5.png" alt="" draggable={false}/>
+                <img src="/client-6.png" alt="" draggable={false}/>
+                <img src="/client-7.png" alt="" draggable={false}/>
+                <img src="/client-8.png" alt="" draggable={false}/>
+                <img src="/client-9.png" alt="" draggable={false}/>
             </div>
         </section>
     )
