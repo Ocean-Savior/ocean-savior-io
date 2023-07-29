3ca6eedd958c3f2f37ca85fb8ac55426b24bf347
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-07-29 18:02:12 +0500

    init

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,14 +6,20 @@ gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
     const aboutContent = useRef(null);
+    const aboutImage = useRef(null);
     const missionContent = useRef(null);
+    const missionImage = useRef(null);
 
     useEffect(() => {
         const aboutContentAnimation = aboutContent.current;
+        const aboutImageAnimation = aboutImage.current;
         const missionContentAnimation = missionContent.current;
+        const missionImageAnimation = missionImage.current;
 
         gsap.set(aboutContentAnimation, { opacity: 0, xPercent: -20 });
+        gsap.set(aboutImageAnimation, { opacity: 0, xPercent: 20 });
         gsap.set(missionContentAnimation, { opacity: 0, xPercent: 20 });
+        gsap.set(missionImageAnimation, { opacity: 0, xPercent: -20 });
 
         gsap.to(aboutContentAnimation, {
             opacity: 1,
@@ -21,6 +27,18 @@ const About = () => {
             duration: 0.7,
             scrollTrigger: {
                 trigger: aboutContentAnimation,
+                start: "top 80%", 
+                end: "bottom 20%",
+                toggleActions: "play none none none",
+            },
+        });
+
+        gsap.to(aboutImageAnimation, {
+            opacity: 1,
+            xPercent: 0,
+            duration: 0.7,
+            scrollTrigger: {
+                trigger: aboutImageAnimation,
                 start: "top 80%", 
                 end: "bottom 20%",
                 toggleActions: "play none none none",
@@ -38,6 +56,18 @@ const About = () => {
                 toggleActions: "play none none none",
             },
         });
+        
+        gsap.to(missionImageAnimation, {
+            opacity: 1,
+            xPercent: 0,
+            duration: 0.7,
+            scrollTrigger: {
+                trigger: missionImageAnimation,
+                start: "top 80%", 
+                end: "bottom 20%",
+                toggleActions: "play none none none",
+            },
+        });
     }, []);
 
     return (
@@ -49,7 +79,7 @@ const About = () => {
                         <p className={styles.p}>OceanSavior (OSR) is a cryptocurrency with a special focus on the protection and preservation of the world's oceans. Our mission is to engage the global community in addressing critical issues of ocean pollution and the threat to marine ecosystems, stimulating innovation, and supporting projects aimed at cleaning and restoring the oceanic environment.</p>
                         <a href="#presale" className={`btn2 ${styles.btn}`}>Whitepaper</a>
                     </div>
-                    <div className={styles.img}>
+                    <div className={styles.img} ref={aboutImage}>
                         <img src="/about.png" alt=""/>
                     </div>
                 </div>
@@ -61,7 +91,7 @@ const About = () => {
                         <p className={styles.p}>Our mission is to contribute to the creation of healthy, clean, and sustainable oceans for future generations. We aim to be a catalyst for change, mobilizing investments, resources, and ideas in service of the common goal - the protection of our global oceans from pollution and destruction.</p>
                         <a href="#presale" className={`btn2 ${styles.btn}`}>Whitepaper</a>
                     </div>
-                    <div className={styles.img}>
+                    <div className={styles.img} ref={missionImage}>
                         <img src="/mission.png" alt=""/>
                     </div>
                 </div>
